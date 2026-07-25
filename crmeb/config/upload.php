1414e22f142d7d3fd077aa23a80d4e827a55e55e
xhtxzc <hegroundnut@gmail.com>
2026-07-26 00:47:56 +0800

    优化前端

--- crmeb/config/upload.php
+++ crmeb/config/upload.php
@@ -12,8 +12,8 @@
 return [
     //默认上传模式,后台配置优先,添加类型一定索引要和驱动名一致 用小写字母
     'default' => 'local',
-    //上传文件大小 50M
-    'filesize' => 52428800,
+    //上传文件大小 2G
+    'filesize' => 2147483648,
     //上传文件后缀类型
     'fileExt' => ['jpg', 'jpeg', 'png', 'gif', 'pem', 'mp3', 'wma', 'wav', 'amr', 'mp4', 'key', 'xlsx', 'xls', 'txt', 'ico', 'crt', 'webp', 'zip'],
     //上传文件类型
